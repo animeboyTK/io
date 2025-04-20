153b621972c94b39c6b7c85237c2bb0189f03700
animeboyTK <purapuraacc@gmail.com>
2025-04-20 18:37:57 +0800

    Add files via upload

---
+++ episodes.js
@@ -0,0 +1,6 @@
+
+const episodes = [
+  { number: 1125, video: "https://streamtape.com/e/xxxxxxx", download: "https://shrinke.me/onepiece1125" },
+  { number: 1124, video: "https://streamtape.com/e/yyyyyyy", download: "https://shrinke.me/onepiece1124" },
+  { number: 1123, video: "https://streamtape.com/e/zzzzzzz", download: "https://shrinke.me/onepiece1123" }
+];
